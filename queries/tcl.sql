10a4645f066ca7d5b7ad585fb14cf729d64c72ea
nikiljos <nikhiljose12345@gmail.com>
2023-11-19 22:52:04 +0530

    feat: TCL queries

---
+++ queries/tcl.sql
@@ -0,0 +1,18 @@
+-- start a transaction
+START TRANSACTION;
+
+-- sample insertion query for demo
+insert into Authors (name)
+values ('Dan Brown');
+
+-- create a savepoiutn with current changes
+SAVEPOINT sp_ins;
+
+-- sample deletion query for demo
+delete from Authors where id=3;
+
+-- rollback to savepoint
+ROLLBACK TO sp_ins;
+
+-- commit changes
+COMMIT;
